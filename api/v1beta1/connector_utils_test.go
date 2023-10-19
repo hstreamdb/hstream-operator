7b9f292a9ebc6810f6fe4556841f3b726e41f8cb
Yue Yang <g1enyy0ung@gmail.com>
2023-10-19 15:44:38 +0800

    chore: add tests

---
+++ api/v1beta1/connector_utils_test.go
@@ -0,0 +1,29 @@
+package v1beta1_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/hstreamdb/hstream-operator/api/v1beta1"
+)
+
+var _ = Describe("v1beta1/connector_utils", func() {
+	It("should generate correct configmap name", func() {
+		connectorTplName := "test-connector-tpl"
+		connectorName := "test-connector"
+
+		configMap1 := v1beta1.GenConnectorConfigMapName(connectorTplName, true)
+		Expect(configMap1).To(Equal("test-connector-tpl-hstream-io-connector-template-config"))
+
+		configMap2 := v1beta1.GenConnectorConfigMapName(connectorName, false)
+		Expect(configMap2).To(Equal("test-connector-hstream-io-connector-config"))
+	})
+
+	It("should generate correct deployment name", func() {
+		connectorName := "test-connector"
+		stream := "test-stream"
+
+		deploymentName := v1beta1.GenConnectorDeploymentName(connectorName, stream)
+		Expect(deploymentName).To(Equal("test-connector-test-stream-hstream-io-connector-depolyment"))
+	})
+})
