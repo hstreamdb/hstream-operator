53b7a85dcf4a8a0d6bdc371d6acca860ad996886
Yue Yang <g1enyy0ung@gmail.com>
2023-11-07 10:23:00 +0800

    fix: gateway port can be omitted (#65)

--- api/v1alpha2/gateway.go
+++ api/v1alpha2/gateway.go
@@ -7,7 +7,7 @@ type Gateway struct {
 	//+kubebuilder:validation:Required
 	Endpoint string `json:"endpoint"`
 	//+kubebuilder:default:=14789
-	Port int32 `json:"port"`
+	Port int32 `json:"port,omitempty"`
 	// Must 'kubernetes.io/tls' secret, and the tls.key must the PKCS8 format
 	SecretRef *corev1.LocalObjectReference `json:"secretRef,omitempty"`
 }
